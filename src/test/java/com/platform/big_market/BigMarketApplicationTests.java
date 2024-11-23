4c1dc209a2066a875c904ee01459b04586c34d0a
kelin.zrh <kelin.zrh@alibaba-inc.com>
2024-11-23 21:11:11 +0800

    feat: init project

---
+++ src/test/java/com/platform/big_market/BigMarketApplicationTests.java
@@ -0,0 +1,13 @@
+package com.platform.big_market;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BigMarketApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
