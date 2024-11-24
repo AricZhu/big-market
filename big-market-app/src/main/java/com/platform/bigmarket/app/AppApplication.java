f2dde3485c0467bb5ab93901e697c2f16f86d3ed
kelin.zrh <kelin.zrh@alibaba-inc.com>
2024-11-24 08:29:18 +0800

    feat: start app

---
+++ big-market-app/src/main/java/com/platform/bigmarket/app/AppApplication.java
@@ -0,0 +1,11 @@
+package com.platform.bigmarket.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(AppApplication.class, args);
+    }
+}
