f7209b329e742ce5f5b8ecaae5d16c35b2f436dd
kelin.zrh <kelin.zrh@alibaba-inc.com>
2024-11-28 10:56:40 +0800

    feat: 完成其他表的持久层处理

--- big-market-app/src/test/java/com/platform/bigmarket/test/InfrastructureTest.java
+++ big-market-app/src/test/java/com/platform/bigmarket/test/InfrastructureTest.java
@@ -2,7 +2,13 @@ package com.platform.bigmarket.test;
 
 import com.alibaba.fastjson.JSON;
 import com.platform.bigmarket.infrastructure.persistent.dao.IAwardDao;
+import com.platform.bigmarket.infrastructure.persistent.dao.IStrategyAwardDao;
+import com.platform.bigmarket.infrastructure.persistent.dao.IStrategyDao;
+import com.platform.bigmarket.infrastructure.persistent.dao.IStrategyRuleDao;
 import com.platform.bigmarket.infrastructure.persistent.po.Award;
+import com.platform.bigmarket.infrastructure.persistent.po.Strategy;
+import com.platform.bigmarket.infrastructure.persistent.po.StrategyAward;
+import com.platform.bigmarket.infrastructure.persistent.po.StrategyRule;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -19,9 +25,37 @@ public class InfrastructureTest {
     @Autowired
     private IAwardDao awardDao;
 
+    @Autowired
+    private IStrategyDao strategyDao;
+
+    @Autowired
+    private IStrategyAwardDao strategyAwardDao;
+
+    @Autowired
+    private IStrategyRuleDao strategyRuleDao;
+
     @Test
     public void test_queryAwardList() {
         List<Award> awards = awardDao.queryAwardList();
         log.info("query award list: {}", JSON.toJSONString(awards));
+    }
+
+    @Test
+    public void test_queryStrategyList() {
+        List<Strategy> strategies = strategyDao.queryStrategyList();
+        log.info("query strategie list: {}", JSON.toJSONString(strategies));
+    }
+
+    @Test
+    public void test_queryStrategyAwardList() {
+        List<StrategyAward> strategyAwards = strategyAwardDao.queryStrategyAwardList();
+        log.info("query strategyAward list: {}", JSON.toJSONString(strategyAwards));
+    }
+
+    @Test
+    public void test_queryStrategyRuleList() {
+        List<StrategyRule> strategyRules = strategyRuleDao.queryStrategyRuleList();
+        log.info("query strategyRule list: {}", JSON.toJSONString(strategyRules));
+
     }
 }
