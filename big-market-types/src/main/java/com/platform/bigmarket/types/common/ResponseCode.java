516055ff7578d56e344f2574dc698092de05e04c
kelin.zrh <kelin.zrh@alibaba-inc.com>
2024-12-17 09:57:41 +0800

    feat: 修改响应成功状态码

--- big-market-types/src/main/java/com/platform/bigmarket/types/common/ResponseCode.java
+++ big-market-types/src/main/java/com/platform/bigmarket/types/common/ResponseCode.java
@@ -7,7 +7,7 @@ import lombok.Getter;
 @Getter
 @AllArgsConstructor
 public enum ResponseCode {
-    RESPONSE_SUCCESS("1001", "成功"),
+    RESPONSE_SUCCESS("0000", "成功"),
     RESPONSE_ERROR("5001", "失败")
     ;
     private String code;
